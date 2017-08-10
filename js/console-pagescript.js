db9ff77735b37096f827cb844b4fb6b202f1fd76
vi-v <vvijayan@purdue.edu>
2017-08-10 13:53:58 +0530

    Update advisor console

--- js/console-pagescript.js
+++ js/console-pagescript.js
@@ -1,6 +1,5 @@
 $(document).ready(() => {
     $('#student-data-container').empty()
-    showStudentData()
 
     $('#risk-button-1').click(() => {
         $('#tab-header').removeClass('bg-danger').removeClass('bg-success').addClass('teal')
@@ -13,22 +12,95 @@ $(document).ready(() => {
     $('#risk-button-3').click(() => {
         $('#tab-header').removeClass('bg-danger').removeClass('teal').addClass('bg-success')
     })
+
+    $.ajax({
+        dataType: "json",
+        url: 'http://52.14.179.178:4404/api/v1/students',
+        success: function(data) {
+            data.reverse()
+            
+            data.forEach(student => {
+                let studentCard = createStudentCard(student)
+                let clone = studentCard.clone()
+
+                let clickFunc = () => {
+                    showStudentData(studentCard.data)
+                }
+
+                studentCard.click(clickFunc)
+                clone.click(clickFunc)
+
+                $('#panel1').append(studentCard)
+
+                if(studentCard.data.atRisk) {
+                    $('#panel2').append(clone)
+                } else {
+                    $('#panel3').append(clone)
+                }
+            })
+        }
+    });
 })
 
-function showStudentData() {
+function createStudentCard(data) {
+    /*
+    <div class="risk-card waves-effect card p-3 hoverable mt-2 mb-2">
+        <div class="row">
+            <div class="col-xs-6 col-md-3 my-auto text-center">
+                <img src="https://avatars0.githubusercontent.com/u/20342762?v=4&s=400" class="risk-thumbnail z-depth-2">
+            </div>
+            <div class="col-xs-6 col-md-9 my-auto text-xs-center text-sm-center text-lg-left">
+                <h2>Parth Shelgaonkar</h2>
+                <span class="font-weight-bold text-danger">At Risk</span>
+            </div>
+        </div>
+    </div>
+    */
+    let riskBadge = null
+    if(data.atRisk) {
+        riskBadge = $('<span class="font-weight-bold text-danger">At Risk</span>')
+    } else {
+        riskBadge = $('<span class="font-weight-bold text-success">Not At Risk</span>')
+    }
+
+    let card = $('<div>', {class: 'risk-card waves-effect card p-3 hoverable mt-2 mb-2'})
+    let row = $('<div>', { class: 'row' })
+    let thumbnailCol = $('<div class="col-xs-6 col-md-3 my-auto text-center"><img src="images/default_profile.png" class="risk-thumbnail z-depth-2"></div>')
+    let nameCol = $('<div>', { class: 'col-xs-6 col-md-9 my-auto text-xs-center text-sm-center text-lg-left' })
+
+    nameCol.append($('<h2>', { class: "text-capitalize", text:data.name }))
+    nameCol.append(riskBadge)
+
+    row.append(thumbnailCol)
+    row.append(nameCol)
+
+    card.append(row)
+    card.data = data
+
+    return card
+}
+
+function showStudentData(data) {
     let $container = $('#student-data-container')
+    $container.empty()
 
-    $container.append('<h1 class="text-center text-underline mb-4"><span id="student-name-header">'+"Nihar Garg"+'</span></h1>')
+    $container.append('<h1 class="text-center text-underline text-capitalize mb-4"><span id="student-name-header">' + data.name + '</span></h1>')
 
-    let riskRow = $('<div>', {class:"row h-10"})
+    let riskRow = $('<div>', { class: "row h-10" })
     riskRow.append('<span class="h5 student-property my-auto mb-2 mt-2">Risk Status: </span>')
-    riskRow.append($('<span>', { id: "student-risk-status", class: "card bg-danger", text: "At Risk" }))
+
+    if(data.atRisk) {
+        riskRow.append($('<span>', { id: "student-risk-status", class: "card bg-danger", text: "At Risk" }))
+    } else {
+        riskRow.append($('<span>', { id: "student-risk-status", class: "card bg-success", text: "Not At Risk" }))
+    }
+
     riskRow.appendTo($container)
 
-    studentDataFields.forEach( studentData => {
-        let row = $('<div>', {class:"row h-10"})
-        row.append($('<span>', {class: "h5 student-property my-auto mb-2 mt-2", text: studentData.description+":"}))
-        //row.append($('<span>', { id: "student-risk-status", class: "card bg-danger", text: "At Risk" }))
+    studentDataFields.forEach(studentData => {
+        let row = $('<div>', { class: "row h-10" })
+        row.append($('<span>', { class: "h5 student-property my-auto mb-2 mt-2", text: studentData.description + ":" }))
+        row.append($('<span>', { class: "my-auto mt-2", text: studentData.evaluate(data[studentData.label]) }))
         row.appendTo($container)
     })
 }
--- js/consoledata.js
+++ js/consoledata.js
@@ -1,126 +1,434 @@
 var studentDataFields = [
     {
         label: "sex",
-        description: "Sex"
+        description: "Sex",
+        evaluate: (data) => {
+            if (data.toLowerCase() === "f") {
+                return "Female"
+            } else if (data.toLowerCase() === "m") {
+                return "Male"
+            }
+        }
     },
     {
         label: "age",
-        description: "Age"
-    },
-    {
-        label: "address",
-        description: "Home address type"
+        description: "Age",
+        evaluate: (data) => {
+            return data
+        }
     },
     {
         label: "famsize",
-        description: "Family size"
+        description: "Family size",
+        evaluate: (data) => {
+            if( data === "LT3" ) {
+                return "Less than or equal to 3"
+            } else if (data === "GT3") {
+                return "Greater than 3"
+            }
+        }
     },
     {
         label: "Pstatus",
-        description: "Parents' cohabitation status"
+        description: "Parents' cohabitation status",
+        evaluate: (data) => {
+            if (data === "T") {
+                return "Living together"
+            } else {
+                return "Living apart"
+            }
+        }
     },
     {
         label: "Medu",
-        description: "Mother's education"
+        description: "Mother's education",
+        evaluate: (data) => {
+            if (data == "0") {
+                return "None"
+            } else if (data == "1") {
+                return "Primary education"
+            } else if (data == "2") {
+                return "Secondary education"
+            } else if (data >= "3") {
+                return "Higher education"
+            }
+        }
     },
     {
         label: "Fedu",
-        description: "Father's education"
+        description: "Father's education",
+        evaluate: (data) => {
+            if (data == "0") {
+                return "None"
+            } else if (data == "1") {
+                return "Primary education"
+            } else if (data == "2") {
+                return "Secondary education"
+            } else if (data >= "3") {
+                return "Higher education"
+            }
+        }
     },
     {
         label: "Mjob",
-        description: "Mother's job"
+        description: "Mother's job",
+        evaluate: (data) => {
+            if( data == "health") {
+                return "Health"
+            } else if (data == "services") {
+                return "Civil Services"
+            } else if(data == "athome") {
+                return "Stay at home"
+            } else if(data == "other") {
+                return "Other"
+            } else if(data == "teacher") {
+                return "Teacher"
+            }
+        }
     },
     {
         label: "Fjob",
-        description: "Father's job"
+        description: "Father's job",
+        evaluate: (data) => {
+            if( data == "health") {
+                return "Health"
+            } else if (data == "services") {
+                return "Civil Services"
+            } else if(data == "athome") {
+                return "Stay at home"
+            } else if(data == "other") {
+                return "Other"
+            } else if(data == "teacher") {
+                return "Teacher"
+            }
+        }
     },
     {
         label: "reason",
-        description: "Reason to choose this school"
+        description: "Reason to choose this school",
+        evaluate: (data) => {
+            if( data == "home") {
+                return "Close to home"
+            } else if (data == "reputation") {
+                return "School reputation"
+            } else if(data == "course") {
+                return "Course preference"
+            } else if(data == "other") {
+                return "Other"
+            }
+        }
     },
     {
         label: "guardian",
-        description: "Guardian"
+        description: "Guardian",
+        evaluate: (data) => {
+            if( data == "mother") {
+                return "Mother"
+            } else if (data == "father") {
+                return "Father"
+            } else if(data == "other") {
+                return "Other"
+            }
+        }
     },
     {
         label: "traveltime",
-        description: "Home to school travel time"
+        description: "Home to school travel time",
+        evaluate: (data) => {
+            if (data == "1") {
+                return "Less than 15 minutes"
+            } else if (data == "2") {
+                return "15 - 30 minutes"
+            } else if (data == "3") {
+                return "30 minutes to 1 hour"
+            } else if (data >= "4") {
+                return "More than an hour"
+            }
+        }
     },
     {
         label: "studytime",
-        description: "Weekly study time"
+        description: "Weekly study time",
+        evaluate: (data) => {
+            if (data == "1") {
+                return "Less than 2 hours"
+            } else if (data == "2") {
+                return "2 to 5 hours"
+            } else if (data == "3") {
+                return "5 to 10 hours"
+            } else if (data >= "4") {
+                return "More than 10 hours"
+            }
+        }
     },
     {
         label: "failures",
-        description: "Number of past class failures"
+        description: "Number of past class failures",
+        evaluate: (data) => {
+            return data
+        }
     },
     {
         label: "schoolsup",
-        description: "Extra educational support"
+        description: "Extra educational support",
+        evaluate: (data) => {
+            if (data == "1") {
+                return "Yes"
+            } else if (data == "2") {
+                return "No"
+            }
+        }
     },
     {
         label: "famsup",
-        description: "Family educational support"
+        description: "Family educational support",
+        evaluate: (data) => {
+            if (data == "1") {
+                return "Yes"
+            } else if (data == "2") {
+                return "No"
+            }
+        }
     },
     {
         label: "paid",
-        description: "Extra paid classes within the course subject"
+        description: "Extra paid classes within the course subject",
+        evaluate: (data) => {
+            if (data == "1") {
+                return "Yes"
+            } else if (data == "0") {
+                return "No"
+            }
+        }
     },
     {
         label: "activities",
-        description: "Extra-curricular activities"
+        description: "Extra-curricular activities",
+        evaluate: (data) => {
+            if (data == "1") {
+                return "Yes"
+            } else if (data == "0") {
+                return "No"
+            }
+        }
     },
     {
         label: "nursery",
-        description: "Attended nursery school"
+        description: "Attended nursery school",
+        evaluate: (data) => {
+            if (data == "1") {
+                return "Yes"
+            } else if (data == "0") {
+                return "No"
+            }
+        }
     },
     {
         label: "higher",
-        description: "Wants to take higher education"
+        description: "Wants to take higher education",
+        evaluate: (data) => {
+            if (data == "1") {
+                return "Yes"
+            } else if (data == "0") {
+                return "No"
+            }
+        }
     },
     {
         label: "internet",
-        description: "Access to internet at home"
+        description: "Access to internet at home",
+        evaluate: (data) => {
+            if (data == "1") {
+                return "Yes"
+            } else if (data == "0") {
+                return "No"
+            }
+        }
     },
     {
         label: "romantic",
-        description: "In a romantic relationship"
+        description: "In a romantic relationship",
+        evaluate: (data) => {
+            if (data == "1") {
+                return "Yes"
+            } else if (data == "0") {
+                return "No"
+            }
+        }
     },
     {
         label: "famrel",
-        description: "Quality of family relationships"
+        description: "Quality of family relationships",
+        evaluate: (data) => {
+            if ( data == "1" ) {
+                return "Very bad"
+            } else if (data == "2") {
+                return "Bad"
+            } else if (data == "3") {
+                return "Average"
+            } else if (data == "4") {
+                return "Good"
+            } else if (data == "5") {
+                return "Very good"
+            }
+        }
     },
     {
         label: "freetime",
-        description: "Free time after school"
+        description: "Free time after school",
+        evaluate: (data) => {
+            if ( data == "1" ) {
+                return "Very low"
+            } else if (data == "2") {
+                return "Low"
+            } else if (data == "3") {
+                return "Average"
+            } else if (data == "4") {
+                return "High"
+            } else if (data == "5") {
+                return "Very high"
+            }
+        }
     },
     {
         label: "goout",
-        description: "Social events with friends"
+        description: "Social events with friends",
+        evaluate: (data) => {
+            if ( data == "1" ) {
+                return "Very low"
+            } else if (data == "2") {
+                return "Low"
+            } else if (data == "3") {
+                return "Average"
+            } else if (data == "4") {
+                return "High"
+            } else if (data == "5") {
+                return "Very high"
+            }
+        }
     },
     {
         label: "Dalc",
-        description: "Workday alcohol consumption"
+        description: "Workday alcohol consumption",
+        evaluate: (data) => {
+            if ( data == "1" ) {
+                return "Very low"
+            } else if (data == "2") {
+                return "Low"
+            } else if (data == "3") {
+                return "Average"
+            } else if (data == "4") {
+                return "High"
+            } else if (data == "5") {
+                return "Very high"
+            }
+        }
     },
     {
         label: "Walc",
-        description: "Weekend alcohol consumption"
+        description: "Weekend alcohol consumption",
+        evaluate: (data) => {
+            if ( data == "1" ) {
+                return "Very low"
+            } else if (data == "2") {
+                return "Low"
+            } else if (data == "3") {
+                return "Average"
+            } else if (data == "4") {
+                return "High"
+            } else if (data == "5") {
+                return "Very high"
+            }
+        }
     },
     {
         label: "health",
-        description: "Current health status"
+        description: "Current health status",
+        evaluate: (data) => {
+            if ( data == "1" ) {
+                return "Very bad"
+            } else if (data == "2") {
+                return "Bad"
+            } else if (data == "3") {
+                return "Average"
+            } else if (data == "4") {
+                return "Good"
+            } else if (data == "5") {
+                return "Very good"
+            }
+        }
     },
     {
         label: "absences",
-        description: "Number of school absences"
+        description: "Number of school absences",
+        evaluate: (data) => {
+            return data
+        }
     },
     {
         label: "G1",
-        description: "First period grade"
+        description: "First semester grade",
+        evaluate: (data) => {
+            data = Number(data)*5
+
+            if ( data >= "97") {
+                return "A+"
+            } else if ( data >= "93") {
+                return "A"
+            } else if (data >= "90") {
+                return "A-"
+            } else if (data >= "87") {
+                return "B+"
+            } else if (data >= "83") {
+                return "B"
+            } else if (data >= "80") {
+                return "B-"
+            } else if (data >= "77") {
+                return "C+"
+            } else if (data >= "73") {
+                return "C"
+            } else if (data >= "70") {
+                return "C-"
+            } else if (data >= "67") {
+                return "D+"
+            } else if (data >= "65") {
+                return "D"
+            } else if (data < "65") {
+                return "F"
+            }
+        }
     },
     {
         label: "G2",
-        description: "Second period grade"
+        description: "Second semester grade",
+        evaluate: (data) => {
+            data = Number(data)*5
+
+            if ( data >= "97") {
+                return "A+"
+            } else if ( data >= "93") {
+                return "A"
+            } else if (data >= "90") {
+                return "A-"
+            } else if (data >= "87") {
+                return "B+"
+            } else if (data >= "83") {
+                return "B"
+            } else if (data >= "80") {
+                return "B-"
+            } else if (data >= "77") {
+                return "C+"
+            } else if (data >= "73") {
+                return "C"
+            } else if (data >= "70") {
+                return "C-"
+            } else if (data >= "67") {
+                return "D+"
+            } else if (data >= "65") {
+                return "D"
+            } else if (data < "65") {
+                return "F"
+            }
+        }
     }
 ]
